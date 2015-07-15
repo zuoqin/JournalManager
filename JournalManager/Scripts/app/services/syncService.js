69064748849c8aeae4d50a1489276468aa9d4e8a
zuoqin <zuoqinr@163.com>
2015-07-15 14:28:14 +0800

    Sync one by one instead of aray

--- JournalManager/Scripts/app/services/syncService.js
+++ JournalManager/Scripts/app/services/syncService.js
@@ -39,16 +39,19 @@
                 sync: function() {
                     var deferred = $q.defer();
                     localDBService.open(dbModel).then(function() {
-                        localDBService.getAll(dbModel.objectStoreName).then(function(homes) {
-                            remotePersistenceStrategy.save(homes).then(function(result) {
-                                if (result) {
-                                    localDBService.clear(dbModel.objectStoreName).then(function(res) {
-                                        deferred.resolve(true);
-                                    }, deferred.reject);
-                                } else {
-                                    deferred.reject('Unable to clear object store');
-                                }
-                            }, deferred.reject);
+                        localDBService.getAll(dbModel.objectStoreName).then(function (items) {
+                            items.forEach(function(item) {
+                                remotePersistenceStrategy.save(item).then(function (result) {
+                                    if (result) {
+                                        localDBService.clear(dbModel.objectStoreName).then(function(res) {
+                                            deferred.resolve(true);
+                                        }, deferred.reject);
+                                    } else {
+                                        deferred.reject('Unable to clear object store');
+                                    }
+                                }, deferred.reject);
+                            });
+
                         }, deferred.reject);
                     }, deferred.reject);
                     return deferred.promise;
